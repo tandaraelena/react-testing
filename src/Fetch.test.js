fc720cb9a5d26dd7855f7279816a7c5ea425eae1
tandaraelena <tandaraelena@yahoo.com>
2019-10-27 13:14:36 +0000

    await for data to be fetched and test it

--- src/Fetch.test.js
+++ src/Fetch.test.js
@@ -7,8 +7,16 @@ import axiosMock from 'axios'
 afterEach(cleanup)
 
 it('fetches and displays data', async () => {
+  axiosMock.get.mockResolvedValueOnce({ data: { greeting: 'hello and bye' }})
+
   const url = '/greeting';
   const { getByTestId } = render(<Fetch url={url} />);
 
-  expect(getByTestId('loading')).toHaveTextContent('Loading data...')
+  expect(getByTestId('loading')).toHaveTextContent('Loading data...');
+
+  const resolvedSpan = await waitForElement(() => getByTestId('resolved'));
+
+  expect(resolvedSpan).toHaveTextContent('hello and bye');
+  expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  expect(axiosMock.get).toHaveBeenCalledWith(url)
 });
